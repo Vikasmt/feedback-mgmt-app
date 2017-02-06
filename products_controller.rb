f73b7ab98561a356a834b5962a1bcf82829e4396
vikas <vikas.y@moldtekindia.com>
2017-02-06 15:56:57 +0530

    Create products_controller.rb

---
+++ products_controller.rb
@@ -0,0 +1,9 @@
+before_filter :authenticate
+
+protected
+
+def authenticate
+  authenticate_or_request_with_http_basic do |username, password|
+    username == "foo" && password == "bar"
+  end
+end
